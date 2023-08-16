03f31e5cae9cdb2515800276f56cac03ff1db5be
cgwol <cgwalterson@icloud.com>
2023-08-16 15:37:37 -0700

    Completed

--- index.js
+++ index.js
@@ -1,28 +1,5 @@
-// const { writeFile } = require('fs/promises');
-// const { Circle, Square, Triangle } = require('./lib/shapes.js');
 const inq = require('./lib/inq.js');
 const generator = require('./lib/generator.js');
-
-// const circle = new Circle(
-//     'ABC',
-//     'black',
-//     'green'
-// );
-
-// const svg = circle.renderLogoCircle();
-
-// const square = new Square(
-//     '123',
-//     'black',
-//     'gray'
-// );
-
-// const svg = square.renderLogo();
-
-
-// console.log(svg);
-
-// writeFile('./dist/logo.svg', svg);
 
 inq.init().then(responses => {
     console.log(responses);
